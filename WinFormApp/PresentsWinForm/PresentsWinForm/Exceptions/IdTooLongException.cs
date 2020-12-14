b2a3e9b9a5435e141bdc6716145e334fa74b6af6
SzaSzami <szabcsing12@gmail.com>
2020-12-14 14:04:59 +0100

    WinFormApp added
    This is the base of the program.

---
+++ WinFormApp/PresentsWinForm/PresentsWinForm/Exceptions/IdTooLongException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Presents.Models.Records
+{
+    [Serializable]
+    internal class IdTooLongException : Exception
+    {
+        public IdTooLongException()
+        {
+        }
+
+        public IdTooLongException(string message) : base(message)
+        {
+        }
+
+        public IdTooLongException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IdTooLongException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ WinFormApp/PresentsWinForm/PresentsWinForm/Exceptions/PresentNameNotValidException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Presents.Models.Records
+{
+    [Serializable]
+    internal class PresentNameNotValidException : Exception
+    {
+        public PresentNameNotValidException()
+        {
+        }
+
+        public PresentNameNotValidException(string message) : base(message)
+        {
+        }
+
+        public PresentNameNotValidException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected PresentNameNotValidException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
